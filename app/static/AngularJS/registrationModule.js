40a12dcb550d8be540be267bdb84342c8245dbe8
GibranQC <gibranqc9131@gmail.com>
2017-02-21 12:15:00 -0600

    Nuevo Proyecto

---
+++ app/static/AngularJS/registrationModule.js
@@ -0,0 +1,40 @@
+// -- =============================================
+// -- Author:      Gibran 
+// -- Create date: 05/09/2016
+// -- Description: Is the container of the application
+// -- Modificó: 
+// -- Fecha: 
+// -- =============================================
+var registrationModule = angular.module("registrationModule", ["ngRoute", "LocalStorageModule"])
+    .config(function($routeProvider, $locationProvider) {
+
+        /*cheange the routes*/
+        $routeProvider.when('/', {
+            templateUrl: 'AngularJS/Templates/login.html', //example 1
+        //     params: {
+        //     mostrar: true
+        // }
+            controller: 'loginController'
+        });
+        $routeProvider.otherwise({redirectTo:'/'});
+
+        $locationProvider.html5Mode({
+            enabled: true,
+            requireBase: false
+        });
+    });
+
+registrationModule.directive('resize', function($window) {
+    return function(scope, element) {
+        var w = angular.element($window);
+        var changeHeight = function() { element.css('height', (w.height() - 20) + 'px'); };
+        w.bind('resize', function() {
+            changeHeight(); // when window size gets changed             
+        });
+        changeHeight(); // when page loads          
+    };
+});
+registrationModule.run(function($rootScope) {
+    $rootScope.var = "full";
+
+})
